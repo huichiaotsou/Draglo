460cd17aa2fcbac1816bb406da3d2887fa70054b
Aaron TSOU <huichiao.tsou@gmail.com>
2021-05-14 20:49:42 +0800

    add MVC folders and routers

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
+app.use('/',
+    //rateLimiter,
+    //authenticator(ROLE),
+    [
+        require('./server/route/automation_route')
+    ]
+)
 
 app.listen(4000, ()=>{
     console.log('app running on port 4000');
 })
 
+app.use((err, req, res, next)=>{
+    console.log(err);
+    res.status(500).send(err);
+})
+
+app.use((req, res)=>{
+    res.status(404).send('the page does not exist');
+})
