ccca1eb9f52116dd5c16de7059dfaea6633b95a8
Huichiao (Aaron) <76254323+huichiaotsou@users.noreply.github.com>
2021-05-25 12:54:00 +0800

    same as last commit

--- utils/google.js
+++ utils/google.js
@@ -5,7 +5,7 @@ let directionAPI = async (prevSpotId, nextSpotId, spotsInfo) => {
     let latitude = spotsInfo[prevSpotId].vector[0]
     let longtitude = spotsInfo[prevSpotId].vector[1]
     let timestamp = new Date()
-    let timezoneURL = process.env.TIMEZONE_API + latitude +',' + longtitude + '&timestamp=' + timestamp.setDate(timestamp.getDate() + 2).toString().slice(0,-3)
+    let timezoneURL = process.env.TIMEZONE_API + latitude.toString() +',' + longtitude.toString() + '&timestamp=' + timestamp.setDate(timestamp.getDate() + 2).toString().slice(0,-3)
     console.log("timezoneURL: ");
     console.log(timezoneURL);
 
@@ -20,11 +20,11 @@ let directionAPI = async (prevSpotId, nextSpotId, spotsInfo) => {
     console.log(timezoneOffset);
     
     timestamp.setHours(8,0,0,0)
-    let destinationTime = timestamp.setHours(timestamp.getHours() + timezoneOffset);
+    let destinationTime = timestamp.setHours(timestamp.getHours() + timezoneOffset + 8);
     console.log('destinationTime: ');
     console.log(destinationTime);
 
-    let directionURL = process.env.DIRECTION_API + 'origin=place_id:' + prevSpotId +'&destination=place_id:' + nextSpotId + '&departure_time=' + destinationTime
+    let directionURL = process.env.DIRECTION_API + 'origin=place_id:' + prevSpotId +'&destination=place_id:' + nextSpotId + '&departure_time=' + destinationTime.toString().slice(0,-3)
     let travelTime = await axios.get(directionURL)
         .then((res) => {
             let data = res.data;
