f3ad4fd91a68c5fb974c7804af10489e1e26ec3d
Huichiao (Aaron) <76254323+huichiaotsou@users.noreply.github.com>
2021-05-25 12:47:15 +0800

    get timezone from google api and apply in direction, to test on EC2

--- utils/google.js
+++ utils/google.js
@@ -2,24 +2,29 @@ require('dotenv').config();
 const axios = require('axios');
 
 let directionAPI = async (prevSpotId, nextSpotId, spotsInfo) => {
-    // let latitude = spotsInfo[prevSpotId].vector[0]
-    // let longtitude = spotsInfo[prevSpotId].vector[1]
-    // let timestamp = new Date()
-    // let timezoneURL = process.env.TIMEZONE_API + latitude +',' + longtitude + '&timestamp=' + timestamp.setDate(timestamp.getDate() + 2).toString().slice(0,-3)
-    // console.log("timezoneURL: ");
-    // console.log(timezoneURL);
-    // let timezoneOffset = await axios.get(timezoneURL)
-    //     .then((res)=> {
-    //         return res.data.rawOffset / 3600;
-    //     })
-    //     .catch(error => {
-    //         console.log(error);
-    //     })
-    
-    
-    // let destinationTime = timestamp;
+    let latitude = spotsInfo[prevSpotId].vector[0]
+    let longtitude = spotsInfo[prevSpotId].vector[1]
+    let timestamp = new Date()
+    let timezoneURL = process.env.TIMEZONE_API + latitude +',' + longtitude + '&timestamp=' + timestamp.setDate(timestamp.getDate() + 2).toString().slice(0,-3)
+    console.log("timezoneURL: ");
+    console.log(timezoneURL);
 
-    let directionURL = process.env.DIRECTION_API + 'origin=place_id:' + prevSpotId +'&destination=place_id:' + nextSpotId + '&departure_time=' //+ destinationTime
+    let timezoneOffset = await axios.get(timezoneURL)
+        .then((res)=> {
+            return res.data.rawOffset / 3600;
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    console.log('timezoneOffset: ');
+    console.log(timezoneOffset);
+    
+    timestamp.setHours(8,0,0,0)
+    let destinationTime = timestamp.setHours(timestamp.getHours() + timezoneOffset);
+    console.log('destinationTime: ');
+    console.log(destinationTime);
+
+    let directionURL = process.env.DIRECTION_API + 'origin=place_id:' + prevSpotId +'&destination=place_id:' + nextSpotId + '&departure_time=' + destinationTime
     let travelTime = await axios.get(directionURL)
         .then((res) => {
             let data = res.data;
