8faa1da9ac8edd98ed5f3ec98abe18f9a98c4bef
Huichiao (Aaron) <76254323+huichiaotsou@users.noreply.github.com>
2021-05-27 15:26:51 +0800

    startDateDatetime = new Date(startDate.split(GMT)[0])

--- server/controller/automation_controller.js
+++ server/controller/automation_controller.js
@@ -7,7 +7,7 @@ const { getNextSpotId , arrangeNextActivity, removeSpot, findPoleSpotIds } = req
 const calculateTrips = async (req, res, next) => {
     console.log(req.body);
     let { tripId, dayId, googleIds, spotsInfo, tripDuration, startTime, startDate } = req.body;
-    let startDateDatetime = new Date(startDate)
+    let startDateDatetime = new Date(startDate.split('GMT')[0])
     let startDateUnix = startDateDatetime.getTime();
     
     tripDuration = tripDuration + 1;
