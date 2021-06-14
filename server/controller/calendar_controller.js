a73cf27d5048b7346db831a6fc1eb8397fe32496
huichiaotsou <huichiao.tsou@gmail.com>
2021-06-14 20:55:34 +0800

    timezone set to taipei

--- server/controller/calendar_controller.js
+++ server/controller/calendar_controller.js
@@ -1,4 +1,6 @@
 const ical = require('ical-generator');
+const tz = require('@touch4it/ical-timezones');
+
 const path = require('path');
 const Calendar = require('../model/calendar_model')
 require('dotenv').config()
@@ -6,19 +8,21 @@ require('dotenv').config()
 const iCalendarFeed = async (req, res, next) => {
     try {
         let { tripId, tripName, iCalEvents } = req.body;
-    
-        const calendar = ical({name: tripName});
+        const vtimezone = tz.getVtimezone('Asia/Taipei');
+
+        const calendar = ical({
+            name: tripName,
+            timezone: vtimezone,
+            });
+        
         for (let event of iCalEvents) {
             event.location = await Calendar.getSpotAddress(event.googleId);
             delete event.googleId
-            event.timezone = 'Asia/Taipei'
             calendar.createEvent(event);
             console.log('iCalEvent: ');
             console.log(event);
         }  
         let calendarId = await Calendar.generateCalendar(tripId);
-        console.log(calendarId);
-    
         await calendar.save(path.join(__dirname + '../../../public/calendars/' + `./${calendarId}.ical`))
         res.status(200).send(process.env.ROOT_ROUTE + '/calendars' + `/${calendarId}.ical`)
     } catch (error) {
